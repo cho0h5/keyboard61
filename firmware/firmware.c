ea1e53219a26de7e0fa78c77b43fc4c911a34ac4
Youngho Cho <dominoyh5@gmail.com>
2025-11-16 14:01:32 +0900

    test

--- firmware/firmware.c
+++ firmware/firmware.c
@@ -4,13 +4,37 @@
 #include "bsp/board_api.h"
 #include "tusb.h"
 
+#define HID_KEY_Z 0x1D
+
+void send_key(uint8_t keycode)
+{
+    if (!tud_hid_ready()) return;
+
+    uint8_t report[8] = {0};
+    report[2] = keycode;
+    tud_hid_keyboard_report(1, 0, report);
+
+    sleep_ms(10);
+
+    uint8_t empty_report[8] = {0};
+    tud_hid_keyboard_report(1, 0, empty_report);
+}
+
 int main()
 {
     board_init();
     tud_init(BOARD_TUD_RHPORT);
 
+    uint32_t last_press = 0;
+
     while (true) {
         tud_task();
+
+        uint32_t now = board_millis();
+        if (now - last_press >= 2000) {
+            send_key(HID_KEY_Z);
+            last_press = now;
+        }
     }
 }
 
